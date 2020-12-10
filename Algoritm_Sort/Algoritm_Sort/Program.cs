7c257ca2548fe912bd137ea4e2c16e2c6123181c
DESKTOP-DORVABS\Ladro_K <ladro.kim@gmail.com>
2020-12-10 14:01:27 +0900

    201210 Edu_Algoritm - 선택정렬(Selected sort)

---
+++ Algoritm_Sort/Algoritm_Sort/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Algoritm_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] list = { 1, 10, 5, 8, 7, 6, 4, 3, 2, 9 };
+
+            Console.Write("Before : ");
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.Write($"{list[i]} ");
+            }
+            Console.WriteLine("\n");
+
+            Console.Write("After : ");
+            int min;
+            int index = 0;
+            for (int i = 0; i < list.Length; i++)
+            {
+                min = 9999;
+                for (int j = i; j < list.Length; j++)
+                {
+                    if (min > list[j])
+                    {
+                        min = list[j];
+                        index = j;
+                    }
+                }
+                int temp = list[i];
+                list[i] = list[index];
+                list[index] = temp;
+            }
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.Write($"{list[i]} ");
+            }
+
+        }
+    }
+}
